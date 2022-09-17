abf32600d45f4ec4778a17181eb798862f7723e9
Dr.Cazuela <quiroz.benjamin@outlook.es>
2022-09-16 21:55:52 -0300

    añadi gatos

--- evaluacion1/urls.py
+++ evaluacion1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from vistab import views as B
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gatos/', B.tercero),
+    path('masgatos/', B.cuarto),
 ]
